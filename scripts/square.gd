20052c67faad7cb04ef01072bbb64ec70393b87e
Ted Yeates <t3d.y34t3s@gmail.com>
2024-12-29 13:51:47 +0000

    feat: new pieces and movement

--- scripts/square.gd
+++ scripts/square.gd
@@ -4,6 +4,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$SquareSprite.texture = defaultsquare
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
